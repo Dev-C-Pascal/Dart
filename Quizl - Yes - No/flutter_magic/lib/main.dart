44440215a3ac4ec9568e24867c4225200355a387
Dev-C-Pascal <lesnikovevgen7965@gmail.com>
2021-04-11 12:35:22 +0300

    Quzl - Yes - No

---
+++ Quizl - Yes - No/flutter_magic/lib/main.dart
@@ -0,0 +1,59 @@
+// importing flutter and dart packages
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+// Creates a Material App
+void main() => runApp(
+  MaterialApp(
+    home: BallPage(),
+  ),
+);
+
+// Creates a Scaffold with
+// appbar using Stateless widget
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green[100],
+      appBar: AppBar(
+        backgroundColor: Colors.green[600],
+        title: Text('QuizL'),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+// Creates a Stateful widget
+class Ball extends StatefulWidget {
+  Ball({Key key}) : super(key: key);
+
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  @override
+
+// Returns app with centered image Flatbutton
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: () {
+          setState(() {
+
+            // Random.nextInt(n) returns random
+            // integer from 0 to n-1
+            ballNumber = Random().nextInt(5) + 1;
+          });
+        },
+
+        // Adding images
+        child: Image.asset('images/ball$ballNumber.png'),
+      ),
+    );
+  }
+}
